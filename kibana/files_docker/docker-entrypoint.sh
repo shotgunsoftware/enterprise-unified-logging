02da81d5be43e7502a839614f5cdcb395cac46bb
Reece Lyne <lyner@dbnr910qmx4.ads.autodesk.com>
2023-02-17 15:50:33 +0000

    Update ELK to 8.6.1

--- kibana/files_docker/docker-entrypoint.sh
+++ kibana/files_docker/docker-entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-exec /provision.sh &
+#response=""
+while [[ $response != "200" ]]; do
+  echo "Import Saved Objects: Waiting for Kibana..."
+  sleep 5
+  response=$(curl -so /dev/null -w "%{http_code}\n" localhost:5601/api/status) 
+  if [[ $response = "200" ]]; then
+    echo "Import Saved Objects: Kibana ready, starting imports"
+    sleep 10
+    for file in $(ls /var/tmp/saved_objects/*.ndjson); do
+      curl -X POST localhost:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@${file}
+    done
+    echo "Import Saved Objects: Imports completed"
+  fi
+done &
+
 exec /usr/local/bin/kibana-docker
